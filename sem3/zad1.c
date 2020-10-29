b2914da8a35e23bb2630c6c63d23970ac694d16c
MalickiJakub <malickijakub1@gmail.com>
2020-10-29 23:26:26 +0100

    Zadanie 2 sem4

--- sem3/zad1.c
+++ sem3/zad1.c
@@ -2,9 +2,6 @@
 #include<stdlib.h>
 int main(int argc, char *argv[])
 {
-    /*printf("%s \n",argv[1]);
-    tu po prostu testowałem czy dobrze podaję argumenty
-    */
     float a=atof(argv[1]); 
    printf("wartość %f do kwadratu to %f \n",a,a*a);
     return(0);
